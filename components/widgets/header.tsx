bbb2381f87fc09f24015aadcc1779adeee17f6ab
Laurent Houssin <dylhunt46@gmail.com>
2023-09-13 15:29:33 +0200

    Creation composant Logo et modifier composant Header

--- components/widgets/header.tsx
+++ components/widgets/header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Logo from './logo'
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between">
-      <div>logo</div>
-      <div>nav</div>
-      <div>buttons</div>
+    <header className="bg-gray-200">
+      <div className="flex items-center justify-between container h-16">
+        <Logo />
+        <nav>nav</nav>
+        <div>buttons</div>
+      </div>
     </header>
   )
 }
