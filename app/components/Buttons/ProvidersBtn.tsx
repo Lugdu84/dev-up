5b332acae1b16f512617a444d6414c352be7cfa8
Thomas <thomas.chaudru@gmail.com>
2023-09-14 18:37:42 +0200

    Providers logic  implemented + refacto (2 new generic components : providers & providers button) + responsive implemented
    Co-authored-by: Guiceccoli <guiceccoli@gmail.com>
    Co-authored-by: David <Lugdu84@users.noreply.github.com>

---
+++ app/components/Buttons/ProvidersBtn.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+
+type ProvidersBtnProps = {
+  onClick: () => void
+  src: string
+  alt: string
+  width: number
+  height: number
+  name: string
+}
+
+export default function ProvidersBtn({
+  onClick,
+  src,
+  alt,
+  width,
+  height,
+  name,
+}: ProvidersBtnProps) {
+  return (
+    <button
+      className="text-black w-[100%] h-[50px] flex flex justify-center items-center border-[#8c8c8c] border rounded-lg bg-[#edebeb] xl:text-sm lg:text-xs"
+      type="submit"
+      onClick={onClick}
+    >
+      <Image
+        className="mr-2"
+        src={src}
+        width={width}
+        height={height}
+        alt={alt}
+      />
+      {name}
+    </button>
+  )
+}
