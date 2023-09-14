4017c93ffbe57c6f4be39cec3681ca78538f8ccb
Mohammad RAHMANI <mrahmani788@gmail.com>
2023-09-14 14:47:20 +0200

    ~ Edit meta

--- lib/constants.ts
+++ lib/constants.ts
@@ -15,7 +15,8 @@ export const NAVBAR_LINKS: Array<TNavLinks> = [
   },
 ]
 
-export const META = {
+export const SITEMETA = {
   title: 'DEV-UP',
-  description: 'Dev-UP is a learning platform for developers',
+  description:
+    "La plateforme d'entraide pour les développeurs, faite par des développeurs. Que vous envisagiez ou que vous soyez en formation, jeunes diplômés, retrouvez toutes les ressources dont vous avez besoin pour monter en compétence, un accompagnement et une communauté de passionnés. L'entraide est le coeur de Dev-Up! Rejoignez nous sans plus attendre.",
 }
