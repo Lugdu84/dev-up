6d12b0dc38db77cb51466cd6e66035f43afe410f
Thomas <thomas.chaudru@gmail.com>
2023-09-19 18:09:50 +0200

    Working on category text
    Co-authored-by: Guiceccoli <guiceccoli@gmail.com>

--- components/widgets/user-categorie.tsx
+++ components/widgets/user-categorie.tsx
@@ -12,44 +12,68 @@ const categories = [
     title: 'Futur apprenant',
     image: '/assets/noob.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
+      '1) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
   },
   {
     title: 'Apprenant',
     image: '/assets/eleve.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
+      '2) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
   },
   {
     title: 'Junior',
     image: '/assets/developper.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
+      '3) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
   },
 ]
-function UserCategorie() {
+export default function UserCategorie() {
   return (
-    <div className=" bg-red-100 m-10 p-10 flex flex-row space-x-10">
-      {categories.map((categorie) => {
-        const { title, image, description } = categorie
-        return (
-          <div className="bg-blue-100 p-5 flex flex-col items-center space-y-2">
-            <div className=" bg-white w-[100px] h-[100px] rounded-full flex justify-center items-center">
-              <Image
-                src={image}
-                alt={title}
-                width={70}
-                height={70}
-                className="object-contain"
-              />
+    <div className="w-full mb-10 flex flex-col self-center md:w-3/4 lg:w-full lg:justify-center">
+      <div className="bg-slate-200 flex flex-col items-center lg:w-5/6 lg:flex-row lg:justify-center 2xl:w-full">
+        {categories.map((categorie) => {
+          const { title, image } = categorie
+          return (
+            <div className="relative border flex flex-col items-center w-full h-full py-10 border-black md:w-full 2xl:w-1/5">
+              <div className=" bg-white w-[200px] h-[200px] rounded-full flex justify-center items-center cursor-pointer">
+                <Image
+                  src={image}
+                  alt={title}
+                  width={100}
+                  height={100}
+                  className="object-cover"
+                />
+              </div>
+              <h2 className="text-center text-lg mt-3 cursor-pointer">
+                {title}
+              </h2>
             </div>
-            <h2 className="text-center">{title}</h2>
-            <p className="hidden">{description}</p>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
+      <div className="2xl:w-full 2xl:flex 2xl:justify-center border border-red-600">
+        {/* {categories.map((categorie) => {
+          const { description } = categorie 
+        return ( */}
+        <div className="2xl:w-3/5 border p-5 border-black">
+          {/* <p className="absolute">{description}</p> */}
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae
+            aliquam nunc nunc eu nunc. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc
+            aliquet nunc, vitae aliquam nunc nunc eu nunc. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam
+            faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu
+            nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae
+            aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc
+            aliquet nunc, vitae aliquam nunc nunc eu nunc.
+          </p>
+        </div>
+        {/* ) })} */}
+      </div>
     </div>
   )
 }
-
-export default UserCategorie
