24c5843aec7e7a44ff295052267c8a3b11ddca50
Mohammad RAHMANI <mrahmani788@gmail.com>
2023-09-20 10:11:55 +0200

    ~ fix useEffect scroll hook

--- lib/hooks/scrollHook.tsx
+++ lib/hooks/scrollHook.tsx
@@ -5,15 +5,24 @@ import { useEffect, useState } from 'react'
 export default function ScrollHook() {
   const [scrollPos, setScrollPos] = useState(false)
   useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setScrollPos(true)
+      } else {
+        setScrollPos(false)
+      }
+    }
+
     if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', () => {
-        if (window.pageYOffset > 10) {
-          setScrollPos(true)
-        } else {
-          setScrollPos(false)
-        }
-      })
+      window.addEventListener('scroll', handleScroll)
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', handleScroll)
+      }
     }
   }, [])
+
   return { scrollPos }
 }
