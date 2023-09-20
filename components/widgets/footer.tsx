794fea1981bfd6a9dd44e15c8ee7c02799527570
Mohammad RAHMANI <mrahmani788@gmail.com>
2023-09-20 12:02:28 +0200

    + Change menu navbar + Edit Color menu icon + add copyright footer
    Co-authored-by: Dylhunt46 <Dylhunt46@users.noreply.github.com>

--- components/widgets/footer.tsx
+++ components/widgets/footer.tsx
@@ -1,11 +1,11 @@
 import IconFooter from '@/components/bases/icon-footer'
-import { FOOTER_LINKS, IconsFooter } from '@/lib/constants'
+import { FOOTER } from '@/lib/constants'
 import Logo from '@/components/bases/logo'
 import FooterLink from '@/components/bases/footer-link'
 
 export default function Footer() {
   return (
-    <footer className="border-t border-border py-8">
+    <footer className="border-t border-border pt-8 pb-4 cursor-default">
       <div className="container flex flex-col md:flex-row items-start justify-center md:justify-between gap-8">
         <div className="max-w-xs flex flex-col gap-4">
           <Logo hasSession color="black" />
@@ -17,7 +17,7 @@ export default function Footer() {
         <div className="flex flex-col gap-4 order-2">
           <p className="font-semibold uppercase text-foreground/80">Liens</p>
           <ul className="flex flex-col justify-center gap-2 text-sm md:justify-end order-2">
-            {FOOTER_LINKS.map(({ label, href }) => (
+            {FOOTER.navigate.map(({ label, href }) => (
               <FooterLink key={label} label={label} href={href} />
             ))}
           </ul>
@@ -25,12 +25,15 @@ export default function Footer() {
         <div className="flex flex-col gap-4 order-2">
           <p className="font-semibold uppercase text-foreground/80">Social</p>
           <ul className="flex items-center">
-            {IconsFooter.map(({ icon, href }) => (
+            {FOOTER.icons.map(({ icon, href }) => (
               <IconFooter key={icon} icon={icon} href={href} />
             ))}
           </ul>
         </div>
       </div>
+      <p className="container mt-4 text-center text-sm text-muted-foreground">
+        {FOOTER.copyright}
+      </p>
     </footer>
   )
 }
