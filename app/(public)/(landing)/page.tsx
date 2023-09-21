76220004a3820da6372d522898c8655a20158faf
Guillaume Ceccoli <guiceccoli@gmail.com>
2023-09-21 18:03:10 +0200

    Ajout du drop down

--- app/(public)/(landing)/page.tsx
+++ app/(public)/(landing)/page.tsx
@@ -1,13 +1,13 @@
 import Banner from '@/components/widgets/banner'
 import Description from '@/components/widgets/description'
-// import UserCategorie from '@/components/widgets/user-categorie'
+import UserCategorie from '@/components/widgets/user-categorie'
 
 export default async function Home() {
   return (
     <div className="flex flex-col">
       <Banner />
       <Description />
-      {/* <UserCategorie /> */}
+      <UserCategorie />
     </div>
   )
 }
--- components/widgets/user-categorie.tsx
+++ components/widgets/user-categorie.tsx
@@ -1,35 +1,67 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Image from 'next/image'
 
-const categories = [
+type Category = {
+  id: number
+  title: string
+  image: string
+  description: string
+}
+
+const categories: Category[] = [
   {
+    id: 1,
     title: 'Futur apprenant',
     image: '/assets/noob.png',
     description:
       '1) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
   },
   {
+    id: 2,
     title: 'Apprenant',
     image: '/assets/eleve.png',
     description:
-      '2) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
+      '2) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
   },
   {
+    id: 3,
     title: 'Junior',
     image: '/assets/developper.png',
     description:
-      '3) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
+      '3) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu nunc.',
   },
 ]
 export default function UserCategorie() {
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null,
+  )
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLDivElement>,
+    index: number,
+  ) => {
+    if (e.key === 'Enter') {
+      setSelectedCategory(categories[index])
+    }
+  }
   return (
     <div className="w-full mb-10 flex flex-col self-center md:w-3/4 lg:w-full lg:justify-center">
       <div className="bg-slate-200 flex flex-col items-center lg:w-5/6 lg:flex-row lg:justify-center 2xl:w-full">
-        {categories.map((categorie) => {
-          const { title, image } = categorie
+        {categories.map((category, index) => {
+          const { title, image, id } = category
           return (
-            <div className="relative border flex flex-col items-center w-full h-full py-10 border-black md:w-full 2xl:w-1/5">
-              <div className=" bg-white w-[200px] h-[200px] rounded-full flex justify-center items-center cursor-pointer">
+            <div
+              key={id}
+              className="relative border flex flex-col items-center w-full h-full py-10 border-black md:w-full 2xl:w-1/5"
+            >
+              <div
+                role="button"
+                onKeyDown={(e) => handleKeyDown(e, index)}
+                tabIndex={0}
+                onClick={() => setSelectedCategory(categories[index])}
+                className=" bg-white w-[200px] h-[200px] rounded-full flex justify-center items-center cursor-pointer"
+              >
                 <Image
                   src={image}
                   alt={title}
@@ -38,36 +70,16 @@ export default function UserCategorie() {
                   className="object-cover"
                 />
               </div>
-              <h2 className="text-center text-lg mt-3 cursor-pointer">
-                {title}
-              </h2>
+              <h2 className="text-center text-lg mt-3">{title}</h2>
             </div>
           )
         })}
       </div>
-      <div className="2xl:w-full 2xl:flex 2xl:justify-center border border-red-600">
-        {/* {categories.map((categorie) => {
-          const { description } = categorie 
-        return ( */}
-        <div className="2xl:w-3/5 border p-5 border-black">
-          {/* <p className="absolute">{description}</p> */}
-          <p className="">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae
-            aliquam nunc nunc eu nunc. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc
-            aliquet nunc, vitae aliquam nunc nunc eu nunc. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam
-            faucibus, nunc nunc aliquet nunc, vitae aliquam nunc nunc eu
-            nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            euismod, nisl sed aliquam faucibus, nunc nunc aliquet nunc, vitae
-            aliquam nunc nunc eu nunc.Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Donec euismod, nisl sed aliquam faucibus, nunc nunc
-            aliquet nunc, vitae aliquam nunc nunc eu nunc.
-          </p>
+      {selectedCategory && (
+        <div className="2xl:w-3/5 2xl:flex p-5 2xl:justify-center 2xl:self-center border border-red-600">
+          <p className="w-full">{selectedCategory.description}</p>
         </div>
-        {/* ) })} */}
-      </div>
+      )}
     </div>
   )
 }
