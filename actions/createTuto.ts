c80ca37c58af68e02d1eb5ea30e6898d4a9f6ae1
Thomas <thomas.chaudru@gmail.com>
2023-09-27 18:24:13 +0200

    new tuto page added & refacto on formTuto
    Co-authored-by: David <Lugdu84@users.noreply.github.com>

--- actions/createTuto.ts
+++ actions/createTuto.ts
@@ -1,35 +1,54 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable import/prefer-default-export */
 
 'use server'
 
 import { Level, Role } from '@prisma/client'
 import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 import prisma from '@/lib/prisma/prisma'
 import options from '@/app/api/auth/[...nextauth]/options'
+import { ErrorsTuto } from '@/types/types'
 
-type tag = 'typescript' | 'javascript' | 'html'
+const getTags = (e: FormData) => {
+  const tags: string[] = []
+  for (const [key, value] of e.entries()) {
+    if (
+      !key.includes('$ACTION') &&
+      value === 'on' &&
+      key !== 'newbie' &&
+      key !== 'apprentice' &&
+      key !== 'junior'
+    )
+      tags.push(key)
+  }
+  return tags
+}
 
 export const createTuto = async (e: FormData) => {
   const session = await getServerSession(options)
+  let errors: ErrorsTuto = { title: '', desc: '', tags: '', level: '' }
   if (!session) return
   const title = e.get('title')?.toString()
   const description = e.get('description')?.toString()
-  const typescript = e.get('typescript')?.toString()
-  const html = e.get('html')?.toString()
-  const javascript = e.get('javascript')?.toString()
   const newbie = e.get('newbie')?.toString()
   const apprentice = e.get('apprentice')?.toString()
   const junior = e.get('junior')?.toString()
-  if (!title || !description) return
-  const tags: tag[] = []
+  if (!title) errors = { ...errors, title: 'Veuillez définir un titre' }
+  if (!description)
+    errors = { ...errors, desc: 'Veuillez définir une descritpion' }
   const level: Level[] = []
-  if (typescript) tags.push('typescript')
-  if (html) tags.push('html')
-  if (javascript) tags.push('javascript')
   if (newbie) level.push(Level.NEWBIE)
   if (apprentice) level.push(Level.APPRENTICE)
   if (junior) level.push(Level.JUNIOR)
-
+  if (!level.length)
+    errors = { ...errors, level: 'Veuillez définir au moins un level' }
+  const tags = getTags(e)
+  if (!tags.length)
+    errors = { ...errors, tags: 'Veuillez définir au moins un tag' }
+  console.log('errors', errors)
+  if (Object.keys(errors).length !== 0) return errors
   if (!session.user?.email) return
   const user = await prisma.user.findUnique({
     where: { email: session.user?.email },
@@ -37,12 +56,15 @@ export const createTuto = async (e: FormData) => {
   if (!user || user.role === Role.USER) return
   const tuto = await prisma.tutorial.create({
     data: {
-      title,
-      description,
+      title: title as string,
+      description: description as string,
       tags,
       level,
       authorId: user.id,
     },
   })
-  console.log('tuto =', tuto)
+
+  if (tuto) {
+    redirect(`/create-tuto/${tuto.id}`)
+  }
 }
--- app/(dashboard)/create-tuto/page.tsx
+++ app/(dashboard)/create-tuto/page.tsx
@@ -1,44 +1,13 @@
 import React from 'react'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
-import { Button } from '@/components/ui/button'
-import Checkbox from '@/components/bases/checkbox'
-import { createTuto } from '@/actions/createTuto'
+import FormTuto from '@/components/widgets/formTuto'
 
 export default function CreateTuto() {
   return (
     <div className="w-full flex flex-col items-center">
-      <h1 className="self-center text-xl my-10">Nouveau tuto</h1>
-      <form action={createTuto} className="w-11/12">
-        <Input
-          name="title"
-          className="w-full"
-          type="text"
-          placeholder="Votre titre"
-          required
-        />
-        <h2 className="mt-6">Votre niveau (plusieurs choix possibles)</h2>
-        <div className="my-2 p-1 flex flex-col">
-          <Checkbox name="newbie" text=" newbie" />
-          <Checkbox name="apprentice" text=" apprentice" />
-          <Checkbox name="junior" text=" junior" />
-        </div>
-        <h2 className="mt-6">Tags (plusieurs choix possibles)</h2>
-        <div className="my-2 p-1 flex flex-col">
-          <Checkbox name="html" text=" HTML" />
-          <Checkbox name="javascript" text=" JavaScript" />
-          <Checkbox name="typescript" text=" Typescript" />
-        </div>
-
-        <Textarea
-          name="description"
-          className="w-full mt-6"
-          placeholder="Votre description"
-          required
-        />
-        <Button className="my-6 bg-green-500 text-lg w-full ">Créer</Button>
-      </form>
-      <Button className="bg-red-500 text-lg w-11/12">Annuler</Button>
+      <h1 className="self-center text-2xl my-10 xl:text-3xl 2xl:text-4xl">
+        Nouveau tuto
+      </h1>
+      <FormTuto />
     </div>
   )
 }
